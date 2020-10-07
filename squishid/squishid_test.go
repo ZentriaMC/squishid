ae0a8175822c0273a7849258dc749e99c3d8806b
Sven Kadak <sven.kadak@gmail.com>
2020-10-07 13:57:07 +0300

    test "nn9r"

--- squishid/squishid_test.go
+++ squishid/squishid_test.go
@@ -1,6 +1,7 @@
 package squishid
 
 import (
+	"math"
 	"testing"
 )
 
@@ -23,6 +24,11 @@ func TestSquish(t *testing.T) {
 	result = Squish(14242959524133701664)
 	if result != "abcdefghijkmo" {
 		t.Errorf("got: %s, want: abcdefghijkmo", result)
+	}
+
+	result = Squish(math.MaxUint64)
+	if result != "nn9r" {
+		t.Errorf("got: %s, want: a", result)
 	}
 }
 
@@ -50,6 +56,11 @@ func TestRestore(t *testing.T) {
 	result, _ = Restore("abcdefghijkmo")
 	if result != 14242959524133701664 {
 		t.Errorf("got: %d, want: 14242959524133701664", result)
+	}
+
+	result, _ = Restore("nn9r")
+	if result != math.MaxUint64 {
+		t.Errorf("got: %d, want: %d", result, math.MaxUint64)
 	}
 
 	result, err := Restore("nnnae")
